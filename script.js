7c7d599026069cb92a08aa4b06244657c52b5f79
ClauKev <kevinxvdp@gmail.com>
2024-07-25 05:47:18 -0300

    a

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const header = document.querySelector("header");
 
     window.addEventListener("scroll", function() {
-        if (window.scrollY > 700) { 
+        if (window.scrollY > 550) { 
             header.classList.add("scrolled");
         } else {
             header.classList.remove("scrolled");
